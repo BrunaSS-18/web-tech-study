35a3325ff702fed2db257f0131ed4b34dbdeadf9
BrunaSS-18 <bestudante206@gmail.com>
2026-06-03 11:16:08 -0300

    "feat:finalizndo o layout e adicionando js no projeto"

---
+++ form-web/js/script.js
@@ -0,0 +1,17 @@
+/*
+    Criando variáveis
+*/
+const nome = document.querySelector("#nome");
+const button = document.querySelector("button");
+const lista = document.querySelector(".lista");
+
+/*
+    Criando Função
+*/
+button.addEventListener("click", (event) => {
+    event.preventDefault();
+    
+    console.log(nome.value)
+    //incluir itens no HTML
+
+});
