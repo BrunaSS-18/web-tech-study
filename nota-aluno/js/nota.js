21208a9262b4f0f7fa38598d3e824a8266bd0a31
BrunaSS-18 <bestudante206@gmail.com>
2026-06-10 11:32:36 -0300

    feat: mensagem na tela

--- nota-aluno/js/nota.js
+++ nota-aluno/js/nota.js
@@ -2,6 +2,7 @@
 
 const inputNota = document.getElementById("nota");
 const resultado = document.getElementById("resultado");
+const message = document.getElementById("message")
 
 function resultadoNotaClick(event){
     event.preventDefault()
@@ -10,16 +11,20 @@ function resultadoNotaClick(event){
 
     //Validação
     if (inputNotaValue == ""){
-        alert("O campo está vázio")
-
+        //alert("O campo está vázio")
+        message.style.display = "block";
         return false
     }
 
     //Lógica
     if (inputNotaValue >= 6) {
-        alert("Você está aprovado");
+        //alert("Você está aprovado");
+        resultado.innerHTML = "Aprovado"
     } 
     else {
-        alert("Você está reprovado");
+        //alert("Você está reprovado");
+        resultado.innerHTML = "Reprovado"
     }
+
+    inputNota.value = ""
 }
