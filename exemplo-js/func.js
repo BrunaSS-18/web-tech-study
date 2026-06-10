5ce21f88c0f894ddcf72f9ed66d4b3052670f932
BrunaSS-18 <bestudante206@gmail.com>
2026-06-10 08:46:00 -0300

    feat: adicionando html e js

---
+++ exemplo-js/func.js
@@ -0,0 +1,19 @@
+function minhaFuncao() {
+    document.write("Minha Função");
+}
+
+function pularLinha() {
+    document.write("<br><br>");
+}
+
+function mostrarNome(nome) {
+    document.write(nome);
+    pularLinha();
+}
+
+
+minhaFuncao();
+pularLinha();
+minhaFuncao();
+mostrarNome("Bruna");
+mostrarNome("Guilherme");
