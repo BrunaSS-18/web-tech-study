e8d6abd7e735b762bb66844e0f2734267eae2ead
BrunaSS-18 <bestudante206@gmail.com>
2026-06-15 11:35:25 -0300

    feat: exercicio

--- pop-up/js/script.js
+++ pop-up/js/script.js
@@ -9,7 +9,18 @@ popup.addEventListener("click", function(event) {
     const classOfClickElement = event.target.classList[0]
     const classNameListArray = ["popup-close", "popup-link", "popup-wrapper"]
 
-    if(classNameListArray.includes(classOfClickElement)){
+    /*if(classNameListArray.includes(classOfClickElement)){
         popup.classList.remove("d-block")
+    }
+
+    const isClassList = classNameListArray.includes(classOfClickElement)
+
+    if(isClassList){
+        popup.classList.remove("d-block")
+    }
+    */
+
+    if (!event.target.closest('.popup-content')) {
+    popup.classList.remove("d-block");
     }
 })
