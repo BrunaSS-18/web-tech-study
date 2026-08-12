dbcf974ec353216c63db7e5a6e14fd2404718195
BrunaSS-18 <bestudante206@gmail.com>
2026-08-12 10:19:13 -0300

    introdução do react

---
+++ introducao-react/src/App.jsx
@@ -0,0 +1,30 @@
+import Header from "./Components/Header"
+
+function App() {
+
+  return (
+    <>
+
+      <Header />
+
+      <section>
+        <article>
+          <h1>Lorem Ipsum</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, numquam!</p>
+        </article>
+     
+        <article>
+          <h1>Lorem Ipsum</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, numquam!</p>
+        </article>
+        <hr />
+      </section>
+
+      <footer>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, numquam!</p>
+      </footer>
+    </>
+  )
+}
+
+export default App
